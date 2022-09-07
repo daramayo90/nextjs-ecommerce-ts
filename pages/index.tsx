7f55aa1a7d7f337b4978b3f106e54987b36eb995
daramayo90 <da.aramayo1990@gmail.com>
2022-09-07 00:07:05 -0500

    directory structure and implement ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
 
+import { Typography } from '@mui/material';
+
+import { ShopLayout } from '../components/layouts';
+
 const Home: NextPage = () => {
-  return <h1>My Ecommerce</h1>;
+  return (
+    <ShopLayout title={'My Ecommerce - Home'} pageDescription={'Find the best products here'}>
+      <Typography variant='h1' component='h1'>
+        Shop
+      </Typography>{' '}
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        All Products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
