b42e2a0b321e50f383915188a3c3a4974e8b39be
daramayo90 <da.aramayo1990@gmail.com>
2022-09-08 23:30:20 -0500

    slideshow component created for product images

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,7 @@
 import { FC, useMemo, useState } from 'react';
-import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import NextLink from 'next/link';
+
+import { Box, Card, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
 
 import { IProduct } from '../../interfaces';
 
@@ -22,9 +24,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}>
       <Card>
-        <CardActionArea>
-          <CardMedia component='img' image={productImage} alt={product.title} className='fadeIn' />
-        </CardActionArea>
+        <NextLink href='/product/slug' passHref prefetch={false}>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component='img'
+                image={productImage}
+                alt={product.title}
+                className='fadeIn'
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
       <Box sx={{ mt: 1 }} className='fadeIn'>
