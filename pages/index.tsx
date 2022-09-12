00f7fa0f7d9a1d856ce90dbf754598e3be9dddcc
daramayo90 <da.aramayo1990@gmail.com>
2022-09-11 21:35:51 -0500

    show loading page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,13 @@ import type { NextPage } from 'next';
 
 import { Typography } from '@mui/material';
 
+import { useProducts } from '../hooks';
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
-
-import useSWR from 'swr';
-const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json());
+import { FullScreenLoading } from '../components/ui';
 
 const HomePage: NextPage = () => {
-  const { data, error } = useSWR('/api/products', fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
-
-  console.log({ data });
+  const { products, isLoading } = useProducts('/products');
 
   return (
     <ShopLayout title={'My Ecommerce - Home'} pageDescription={'Find the best products here'}>
@@ -27,7 +20,7 @@ const HomePage: NextPage = () => {
         All Products
       </Typography>
 
-      <ProductList products={data} />
+      {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
     </ShopLayout>
   );
 };
