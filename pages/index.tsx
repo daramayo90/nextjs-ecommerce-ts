4718cdb5d878795fd11252d83e3cee320c553f52
daramayo90 <da.aramayo1990@gmail.com>
2022-09-11 21:17:43 -0500

    swr library install: https://swr.vercel.app/

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,17 @@ import { ShopLayout } from '../components/layouts';
 import { initialData } from '../database/products';
 import { ProductList } from '../components/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json());
+
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log({ data });
+
   return (
     <ShopLayout title={'My Ecommerce - Home'} pageDescription={'Find the best products here'}>
       <Typography variant='h1' component='h1'>
@@ -17,9 +27,9 @@ const Home: NextPage = () => {
         All Products
       </Typography>
 
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
