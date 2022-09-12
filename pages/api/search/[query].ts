4246cf5711aa7f72ee339e6f2b4e83c5a7bb9f28
daramayo90 <da.aramayo1990@gmail.com>
2022-09-11 19:55:42 -0500

    search products by slug & title endpoint created

---
+++ pages/api/search/[query].ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
+import { Product } from '../../../models';
+
+type Data = { message: string } | IProduct[];
+
+/** Search by Slug & Title */
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return searchProducts(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Bad request' });
+  }
+}
+
+const searchProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  let { query = '' } = req.query;
+
+  if (query.length === 0) return res.status(400).json({ message: 'Specify the query' });
+
+  query = query.toString().toLowerCase();
+
+  await db.connect();
+
+  /** models: 'productSchema.index({ title: 'text', tags: 'text' });'*/
+  const products = await Product.find({ $text: { $search: query } })
+    .select('title images price inStock slug -_id')
+    .lean();
+
+  await db.disconnect();
+
+  res.status(200).json(products);
+};
