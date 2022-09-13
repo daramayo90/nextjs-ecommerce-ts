81a84cf3252ae29f93aa06d93ad2903e0f82a78e
daramayo90 <da.aramayo1990@gmail.com>
2022-09-12 20:20:59 -0500

    UIContext created

---
+++ context/ui/UIProvider.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactNode, useReducer } from 'react';
+import { UIContext, uiReducer } from './';
+
+interface Props {
+  children: ReactNode;
+}
+export interface UIState {
+  isMenuOpen: boolean;
+}
+
+const UI_INITIAL_STATE: UIState = {
+  isMenuOpen: false,
+};
+
+export const UIProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
+
+  const toggleSideMenu = () => {
+    dispatch({ type: '[UI] - ToggleMenu' });
+  };
+
+  return <UIContext.Provider value={{ ...state, toggleSideMenu }}>{children}</UIContext.Provider>;
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { SWRConfig } from 'swr';
 
 import { lightTheme } from '../themes';
+import { UIProvider } from '../context';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,10 +12,12 @@ function MyApp({ Component, pageProps }: AppProps) {
       value={{
         fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
       }}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <UIProvider>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </UIProvider>
     </SWRConfig>
   );
 }
