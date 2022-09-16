d2923db0fbf45494562a1dac53c2d02da9c6fe99
daramayo90 <da.aramayo1990@gmail.com>
2022-09-16 15:33:38 -0500

    JWT: User model created

---
+++ interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  password?: string; // Information not provided to the Frontend
+  role: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
