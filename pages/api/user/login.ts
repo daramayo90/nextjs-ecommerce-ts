066684ca126c67d0573db8c00bf6a0708b1c94bd
daramayo90 <da.aramayo1990@gmail.com>
2022-09-16 16:50:36 -0500

    JWT: JsonWebToken implemented

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -4,6 +4,7 @@ import bcrypt from 'bcryptjs';
 import { db } from '../../../database';
 
 import { User } from '../../../models';
+import { jwt } from '../../../utils';
 
 type Data =
   | { message: string }
@@ -43,10 +44,11 @@ const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: 'Mail or Password not valid - PASSWORD' });
   }
 
-  const { role, name } = user;
+  const { _id, name, role } = user;
+  const token = jwt.signToken(_id, email);
 
   return res.status(200).json({
-    token: '',
+    token,
     user: { email, role, name },
   });
 };
---
+++ utils/jwt.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+export const signToken = (_id: string, email: string) => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No JWT seed - Check environment variables');
+  }
+
+  return jwt.sign(
+    // payload
+    { _id, email },
+
+    // seed
+    process.env.JWT_SECRET_SEED,
+
+    // options
+    { expiresIn: '30d' },
+  );
+};
