81fd9fb1ca140cfa6641fbffec8355f41b0c7f52
daramayo90 <da.aramayo1990@gmail.com>
2022-09-17 11:56:02 -0500

    Auth Context/Provider/Reducer created

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,22 @@
+import { FC, ReactNode, useReducer } from 'react';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+interface Props {
+  children: ReactNode;
+}
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+};
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  return <AuthContext.Provider value={{ ...state }}>{children}</AuthContext.Provider>;
+};
