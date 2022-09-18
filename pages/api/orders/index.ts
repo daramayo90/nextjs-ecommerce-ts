e9fb0132763740eb0ada619c72bf2fab97804956
daramayo90 <da.aramayo1990@gmail.com>
2022-09-18 17:35:35 -0500

    Orders: create new order and validate if someone made modification in the frontend

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { Order, Product } from '../../../models';
 
-type Data = {
-   message: string;
-};
+type Data = { message: string } | IOrder;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
    switch (req.method) {
@@ -13,7 +15,48 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
          return res.status(400).json({ message: 'Bad request' });
    }
 }
-const createOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-   const body = req.body;
-   return res.status(201).json(body);
+const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+   const { orderItems, total } = req.body as IOrder;
+
+   // Verify an user is logged in
+   const session: any = await getSession({ req });
+   if (!session) return res.status(401).json({ message: 'Unauthorized' });
+
+   // Create an array with the products of the client wants
+   const productIds = orderItems.map((product) => product._id);
+
+   await db.connect();
+
+   const dbProducts = await Product.find({ _id: { $in: productIds } });
+
+   try {
+      // Validate if someone made modifications in the frontend
+      const subTotal = orderItems.reduce((prev, current) => {
+         const currentPrice = dbProducts.find((prod) => prod.id === current._id)!.price;
+         if (!currentPrice) {
+            throw new Error('Verify cart again, product does not exist');
+         }
+         return current.quantity * current.price + prev;
+      }, 0);
+
+      const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+      const backendTotal = subTotal * (taxRate + 1);
+
+      if (total !== backendTotal) {
+         throw new Error('The total does not add up to the amount');
+      }
+
+      // Everything is ok
+      const userId = session.user._id;
+      const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+      await newOrder.save();
+
+      await db.disconnect();
+
+      return res.status(201).json(newOrder);
+   } catch (error) {
+      await db.disconnect();
+      console.log(error);
+      res.status(400).json({ message: 'Review server logs' });
+   }
 };
