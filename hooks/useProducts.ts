e8b2b84e20b622239704f7751e5df8fe5b4b01d7
daramayo90 <da.aramayo1990@gmail.com>
2022-09-20 13:03:28 -0500

    getStaticProps in index page

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -3,6 +3,7 @@ import { IProduct } from '../interfaces';
 
 export const useProducts = (url: string, config: SWRConfiguration = {}) => {
    const { data, error } = useSWR<IProduct[]>(`/api${url}`, config);
+   console.log('DATA', data);
 
    return {
       products: data || [],
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next';
+import type { GetStaticProps } from 'next';
 
 import { Typography } from '@mui/material';
 
@@ -6,9 +7,16 @@ import { useProducts } from '../hooks';
 import { ShopLayout } from '../components/layouts';
 import { ProductList } from '../components/products';
 import { FullScreenLoading } from '../components/ui';
+import { IProduct } from '../interfaces/products';
+import { dbProducts } from '../database';
 
-const HomePage: NextPage = () => {
-   const { products, isLoading } = useProducts('/products');
+interface Props {
+   products: IProduct[];
+   isLoading?: boolean;
+}
+
+const HomePage: NextPage<Props> = ({ products }) => {
+   //const { products, isLoading } = useProducts('/products');
 
    return (
       <ShopLayout title={'My Ecommerce - Home'} pageDescription={'Find the best products here'}>
@@ -20,9 +28,19 @@ const HomePage: NextPage = () => {
             All Products
          </Typography>
 
-         {isLoading ? <FullScreenLoading /> : <ProductList products={products} />}
+         <ProductList products={products} />
+
+         {/* {isLoading ? <FullScreenLoading /> : <ProductList products={products} />} */}
       </ShopLayout>
    );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+   const products = await dbProducts.getAllProducts();
+
+   return {
+      props: { products },
+   };
 };
 
 export default HomePage;
